bb988226ae412fa0ad3a669c36f5c300fb5164b7
Escbrk <69437294+Escbrk@users.noreply.github.com>
2023-12-13 00:31:28 -0700

    upd

---
+++ src/index.js
@@ -0,0 +1,81 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const refs = {
+  input: document.querySelector('[type="text"]'),
+  searchBtn: document.querySelector('[type="submit"]'),
+  jsList: document.querySelector('.js-list'),
+};
+
+const params = {
+  API_KEY: '41227446-81114c3a771220f4777577230',
+  BASE_URL: 'https://pixabay.com/api/',
+};
+
+function getGaleryItems() {
+  return fetch(
+    `${params.BASE_URL}?key=${params.API_KEY}&q=${refs.input.value}&image_type=photo&orientation=horizontal&safesearch=true`
+  ).then(resp => {
+    if (!resp.ok) {
+      throw new Error(resp.statusText);
+    }
+
+    return resp.json();
+  });
+}
+
+refs.searchBtn.addEventListener('click', onSearch);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  getGaleryItems()
+    .then(data => {
+      refs.jsList.innerHTML = createMarkup(data.hits);
+    })
+    .catch(err => console.error(err));
+}
+
+function createMarkup(arr) {
+  if (arr.length === 0) {
+    iziToast.error({
+      title: 'Error',
+      message:
+        'Sorry, there are no images matching your search query. Please try again!',
+      position: 'topRight',
+    });
+  }
+  return arr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `
+    <li>
+        <a href='${largeImageURL}'>
+            <img src="${webformatURL}" alt="${tags}" />
+        </a>
+        <span>Likes: ${likes}</span>
+        <span>Views: ${views}</span>
+        <span>Comments: ${comments}</span>
+        <span>Downloads: ${downloads}</span>
+    </li>`;
+      }
+    )
+    .join(' ');
+}
+
+//         <img src="${largeImageURL}" alt="${tags}" />
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
